9bef938bcc3ea465e9812790ca4ffbf8fe3f5dd3
nawshad-niloy <151645651+nawshad-niloy@users.noreply.github.com>
2024-02-15 00:51:32 +0600

    Add files via upload

---
+++ Input_switch.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string myName = Console.ReadLine();
+            //Console.WriteLine("Hello, "+myName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("Enter an Integer : ");
+            //int myInt = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(myInt);
+            //Console.ReadLine();
+
+            //Console.WriteLine("Enter a double : ");
+            //double myDbl = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine(myDbl);
+            //Console.ReadLine();
+
+            // Console.WriteLine("Enter a float : ");
+            // float myFlt = Convert.ToSingle(Console.ReadLine());
+            // Console.WriteLine(myFlt);
+            // Console.ReadLine();
+
+            //Console.Write("Enter a Character : ");
+            //char myChar = Convert.ToChar(Console.ReadLine());
+            //Console.WriteLine(myChar);
+            //Console.ReadLine();
+
+            // string name = Console.ReadLine();
+            //  Console.WriteLine("User name is : " + name);
+            //  Console.WriteLine("How many Candy You want : ");
+            //  int can = Convert.ToInt32(Console.ReadLine());
+            //  Console.WriteLine($"You will get {can} Candy ");
+
+            // int a = 4;
+            // int b = a;
+            // b += 4;
+            //b -= 4;
+            // b *= 4;
+            // b /= 4;
+            //  Console.WriteLine(b);
+
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(false || false);
+
+            // math class is in system.
+            // int a = Math.Max(50,47);
+            //double a = Math.Sqrt(39);
+            //Console.WriteLine(a);
+
+            //String hello = "I am Niloy";
+            //Console.WriteLine(hello.Length);
+            //// for Uppercase string 
+            //Console.WriteLine(hello.ToUpper());
+            //Console.WriteLine(hello.ToLower());
+
+            //String name = Console.ReadLine();
+            // String money = Console.ReadLine();
+            // Console.WriteLine($"Hello {name},Your balance is {money}");
+            //Console.WriteLine(name[3]);
+            // string nam = "Hello New COder";
+            // Console.WriteLine(nam.IndexOf("COder"));
+            // Console.WriteLine(nam.Substring(6));
+
+            /* Console.WriteLine("Enter your Age :\n1.1-10\n2.10-20\n3.20-30");
+
+            int age =Convert.ToInt32(Console.ReadLine());
+            switch (age)
+            {
+                case 1: Console.WriteLine("You are toddler");
+                    break;
+                case 2: Console.WriteLine("You are Teenager");
+                    break;
+                case 3: Console.WriteLine("You are adult");
+                    break;
+                default: Console.WriteLine("Error!!!");
+                    break;
+            } */
+
+
+        }
+
+
+    }
+}
